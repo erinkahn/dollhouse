7f10766242d22b391e9132d169c78630cedec482
Erin Kahn <erinnicolekahn@gmail.com>
2018-09-18 15:31:48 -0400

    nav burger

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,10 @@
 console.log(`Hello World from app.js!`);
 
+document.querySelector('.burger').addEventListener('click', function () {
+    document.querySelector('.desktop-nav').classList.toggle('show');
+    document.querySelector('.burger').classList.toggle('open');
+});
+
 // var tl = new TimelineMax();
 // tl.fromTo('.footer-text', 1, {opacity: 0, left: -2000});
 //# sourceMappingURL=main.js.map
