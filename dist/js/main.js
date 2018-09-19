006a97b9112d502c00328d20d64a90bdc638ca36
Erin Kahn <erinnicolekahn@gmail.com>
2018-09-19 15:17:45 -0400

    js animation nav

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,8 +3,7 @@ console.log(`Hello World from app.js!`);
 document.querySelector('.burger').addEventListener('click', function () {
     document.querySelector('.desktop-nav').classList.toggle('show');
     document.querySelector('.burger').classList.toggle('open');
-});
 
-// var tl = new TimelineMax();
-// tl.fromTo('.footer-text', 1, {opacity: 0, left: -2000});
+    TweenMax.staggerFrom('.desktop-nav ul li a', 1, { opacity: 0, top: -2000, ease: Power4.easeOut }, 0.1);
+});
 //# sourceMappingURL=main.js.map
